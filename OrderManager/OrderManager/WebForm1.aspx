5de6a5fbe23d140992445ee53db1c614e8bbeaf1
Aiguos <mastem1337@gmail.com>
2017-04-26 13:44:01 +0200

    Added C# asp.net

---
+++ OrderManager/OrderManager/WebForm1.aspx
@@ -0,0 +1,39 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="OrderManager.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:TextBox ID="Item1" runat="server" Width="46px" Text="Item1" ReadOnly="true"></asp:TextBox>
+            &nbsp;&nbsp; Amount:&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="Amount1" runat="server" Height="19px" Width="62px" AutoPostBack="False"></asp:TextBox>
+            &nbsp;&nbsp;&nbsp;            
+        </div>
+
+        <div>
+            <asp:TextBox ID="Item2" runat="server" Width="46px" Text="Item2" ReadOnly="true"></asp:TextBox>
+            &nbsp;&nbsp; Amount:&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="Amount2" runat="server" Height="19px" Width="62px" AutoPostBack="False"></asp:TextBox>
+            &nbsp;&nbsp;&nbsp;            
+        </div>
+
+
+        <div>
+            <asp:TextBox ID="Item3" runat="server" Width="46px" Text="Item3" ReadOnly="true"></asp:TextBox>
+            &nbsp;&nbsp; Amount:&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="Amount3" runat="server" Height="19px" Width="62px" AutoPostBack="False"></asp:TextBox>
+            &nbsp;&nbsp;&nbsp;            
+        </div>
+
+
+        <p>
+            <asp:Button ID="Button1" runat="server" Text="Send" OnClick="Button1_Click" />
+        </p>
+    </form>
+</body>
+</html>
---
+++ OrderManager/OrderManager/WebForm1.aspx.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+
+namespace OrderManager
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            int outPut1;
+            int outPut2;
+            int outPut3;
+
+            if (Int32.TryParse(Amount1.Text, out outPut1) && Int32.TryParse(Amount2.Text, out outPut2) && Int32.TryParse(Amount3.Text, out outPut3))
+            {
+
+                using (XmlWriter writer = XmlWriter.Create(@"D:\Order.xml"))
+                {
+                    writer.WriteStartDocument();
+                    writer.WriteStartElement("Order");
+
+                    writer.WriteElementString("Type", Item1.Text);
+                    writer.WriteElementString("Amount", "" + outPut1);
+
+                    writer.WriteElementString("Type", Item2.Text);
+                    writer.WriteElementString("Amount", "" + outPut2);
+
+                    writer.WriteElementString("Type", Item3.Text);
+                    writer.WriteElementString("Amount", "" + outPut3);
+
+                    writer.WriteEndElement();
+                    writer.WriteEndDocument();
+                }
+
+
+            }
+            else
+            {
+                Amount1.Text = "Error";
+                Amount2.Text = "Error";
+                Amount3.Text = "Error";
+            }
+                
+        }
+
+        protected void AddOrder_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
